93ea7e11222b19a66ae1ade05003f4a16704f2cb
xd-hearst <81932912+xd-hearst@users.noreply.github.com>
2022-10-09 20:23:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ data/siteMetadata.js
@@ -0,0 +1,39 @@
+const siteMetadata = {
+  title: 'Tryout Blog',
+  author: 'Xun Ding',
+  headerTitle: 'Who still blogs?',
+  description: 'A blog of restlessness',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteLogo: '/static/images/logo.png',
+  image: '/static/images/avatar.png',
+  socialBanner: '/static/images/twitter-card.png',
+  email: 'address@yoursite.com',
+  github: 'https://github.com',
+  twitter: 'https://twitter.com/Twitter',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com',
+  locale: 'en-US',
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports plausible, simpleAnalytics, umami or googleAnalytics
+    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
+    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
+  },
+  newsletter: {
+    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
+    // Please add your .env file and modify it according to your selection
+    provider: 'buttondown',
+  },
+  comment: {
+  },
+}
+
+module.exports = siteMetadata
