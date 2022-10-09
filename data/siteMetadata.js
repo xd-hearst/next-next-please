20b5b22f419ef09f8b8532bb442c421ddae97874
xd-hearst <xun.ding@cds-global.com>
2022-10-09 16:09:52 -0500

    update meta

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -12,10 +12,6 @@ const siteMetadata = {
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
   github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
